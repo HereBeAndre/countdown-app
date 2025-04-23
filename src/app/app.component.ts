63def984d0a41263a209987982d2918daabd998f
pizzaguyyy <andrea.tota@quinyx.com>
2025-04-23 21:10:16 +0200

    Install MUI. Update template with MUI components. Add minDate to date picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 const getLocalStorageKey = (key: 'eventName' | 'endDate') =>
   localStorage.getItem(key) || '';
@@ -13,7 +17,14 @@ const INTERVAL = 1000;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -24,6 +35,15 @@ export class AppComponent implements OnInit {
   // TODO: Refine this type - should be of type Timeout | number?
   intervalId: any = null;
 
+  // Only used to set the minDate for the datepicker
+  today = new Date();
+  minDate = new Date(
+    this.today.getFullYear(),
+    this.today.getMonth(),
+    this.today.getDate() + 1
+  );
+
+  // Runs on component mount
   ngOnInit() {
     // Resume countdown if there's data in local storage
     if (this.eventName && this.endDate) {
@@ -32,6 +52,16 @@ export class AppComponent implements OnInit {
   }
 
   // TODO: is onDestroy needed to clear the interval?
+  // ngOnDestroy() {
+  //   if (this.intervalId) {
+  //     clearInterval(this.intervalId);
+  //   }
+  //   if (this.delta && this.delta <= 0) {
+  //     this.delta = null;
+  //     setLocalStorageKey('eventName', '');
+  //     setLocalStorageKey('endDate', '');
+  //   }
+  // }
 
   startCountdown() {
     setLocalStorageKey('eventName', this.eventName);
@@ -42,17 +72,17 @@ export class AppComponent implements OnInit {
       const end = new Date(this.endDate).getTime();
       const delta = end - now;
 
-      if (delta === 0) {
-        this.countdown = 'Event has started!';
-        clearInterval(this.intervalId);
-        return;
-      }
+      // if (this.delta === 0) {
+      //   this.countdown = 'Event has started!';
+      //   clearInterval(this.intervalId);
+      //   // return;
+      // }
 
-      if (delta < 0) {
-        this.countdown = 'Event has ended!';
-        clearInterval(this.intervalId);
-        return;
-      }
+      // if (this.delta < 0) {
+      //   this.countdown = 'Event has ended!';
+      //   clearInterval(this.intervalId);
+      //   // return;
+      // }
 
       // TODO: Could be refactored to a Pipe?
       const days = Math.floor(delta / (1000 * 60 * 60 * 24));
