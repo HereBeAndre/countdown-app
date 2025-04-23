d10a0acd0347d3762a8fc7edfc16e11d189a6cb6
pizzaguyyy <andrea.tota@quinyx.com>
2025-04-23 19:35:30 +0200

    Implement startCountdown function. Add it to form ngSubmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 const getLocalStorageKey = (key: 'eventName' | 'endDate') =>
   localStorage.getItem(key) || '';
+
+const setLocalStorageKey = (key: 'eventName' | 'endDate', value: string) =>
+  localStorage.setItem(key, value);
+
+const INTERVAL = 1000;
 
 @Component({
   selector: 'app-root',
@@ -16,8 +21,47 @@ export class AppComponent implements OnInit {
   eventName = getLocalStorageKey('eventName');
   endDate = getLocalStorageKey('endDate');
   countdown = '';
-  // TODO: Refine this type
-  intervalId: number | null = null;
+  // TODO: Refine this type - should be of type Timeout | number?
+  intervalId: any = null;
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Resume countdown if there's data in local storage
+    if (this.eventName && this.endDate) {
+      this.startCountdown();
+    }
+  }
+
+  // TODO: is onDestroy needed to clear the interval?
+
+  startCountdown() {
+    setLocalStorageKey('eventName', this.eventName);
+    setLocalStorageKey('endDate', this.endDate);
+
+    this.intervalId = setInterval(() => {
+      const now = new Date().getTime();
+      const end = new Date(this.endDate).getTime();
+      const delta = end - now;
+
+      if (delta === 0) {
+        this.countdown = 'Event has started!';
+        clearInterval(this.intervalId);
+        return;
+      }
+
+      if (delta < 0) {
+        this.countdown = 'Event has ended!';
+        clearInterval(this.intervalId);
+        return;
+      }
+
+      // TODO: Could be refactored to a Pipe?
+      const days = Math.floor(delta / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((delta / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((delta / (1000 * 60)) % 60);
+      const seconds = Math.floor((delta / 1000) % 60);
+
+      this.countdown = `${days} days, ${hours} h, ${minutes} m, ${seconds} s`;
+      console.log('Countdown:', this.countdown);
+    }, INTERVAL);
+  }
 }
