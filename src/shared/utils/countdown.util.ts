746e01762042d0cb65307bc46d76a97bfb07d0ed
pizzaguyyy <andrea.tota@quinyx.com>
2025-04-25 20:42:33 +0200

    Add padStart to improve UI

--- src/shared/utils/countdown.util.ts
+++ src/shared/utils/countdown.util.ts
@@ -1,8 +1,15 @@
+const padStart = (target: number) => target.toString().padStart(2, '0');
+
 export const formatCountdown = (delta: number) => {
   const days = Math.floor(delta / (1000 * 60 * 60 * 24));
   const hours = Math.floor((delta / (1000 * 60 * 60)) % 24);
   const minutes = Math.floor((delta / (1000 * 60)) % 60);
   const seconds = Math.floor((delta / 1000) % 60);
 
-  return `${days} days, ${hours} h, ${minutes} m, ${seconds} s`;
+  const dayString = padStart(days);
+  const hourString = padStart(hours);
+  const minuteString = padStart(minutes);
+  const secondString = padStart(seconds);
+
+  return `${dayString} days, ${hourString} h, ${minuteString} m, ${secondString} s`;
 };
