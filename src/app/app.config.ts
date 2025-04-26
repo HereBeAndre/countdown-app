761c06fc3b190f6e0156a4bae93429565ba99334
pizzaguyyy <andrea.tota@quinyx.com>
2025-04-26 14:44:29 +0200

    Clean up app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
-
-// TODO: SPA - probably don't need routes
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-  ],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true })],
 };
