4a0b35cdd4773537d2389299f70b7d058d5a02fc
pizzaguyyy <andrea.tota@quinyx.com>
2025-04-26 15:12:17 +0200

    Fix angular.json

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,19 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it('should display the default event name', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.eventName).toEqual('apply to Natural Cycles');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, countdown-app'
-    );
-  });
 });
